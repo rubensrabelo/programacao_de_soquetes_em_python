461ef5a9e8ae55654e72b9bf99e6f3f3d6b62640
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-02-02 07:27:01 -0300

    Dealing with error handling on the client

--- client.py
+++ client.py
@@ -10,21 +10,24 @@ def run_client():
     # Estabelecendo conexão com o servidor
     client.connect((server_ip, port))
 
-    while True:
-        # Insere a mensagem e envia ao servidor
-        msg = input("Enter a message: ")
-        client.send(msg.encode("utf-8")[:1024])
+    try:
+        while True:
+            # Insere a mensagem e envia ao servidor
+            msg = input("Enter a message: ")
+            client.send(msg.encode("utf-8")[:1024])
 
-        response = client.recv(1024)
-        response = response.decode("utf-8")
+            response = client.recv(1024)
+            response = response.decode("utf-8")
 
-        if response.lower() == "closed":
-            break
+            if response.lower() == "closed":
+                break
 
-        print(f"Received: {response}")
-
-    client.close()
-    print("Connection to server closed")
+            print(f"Received: {response}")
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        client.close()
+        print("Connection to server closed")
 
 
 if __name__ == "__main__":
