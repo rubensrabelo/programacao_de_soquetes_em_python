7ecfc1a62be2beef7f5603fac037005e4783dfbb
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-02-04 15:54:44 -0300

    Adding financial methods in the handle client

--- server/services/handle_client.py
+++ server/services/handle_client.py
@@ -1,8 +1,17 @@
 class HandleClient:
-    def __init__(self, client_socket, addr, user_csv):
+    def __init__(
+            self,
+            client_socket,
+            addr,
+            user_manager,
+            financial_manager,
+            user_id
+            ):
         self.client_socket = client_socket
         self.addr = addr
-        self.user_csv = user_csv
+        self.user_manager = user_manager
+        self.financial_manager = financial_manager
+        self.user_id = user_id
 
     def handle_client(self):
         try:
@@ -69,3 +78,65 @@ class HandleClient:
             self.client_socket.send("denied".encode("utf-8"))
             self.client_socket.close()
             return False
+
+    def add_data(self):
+        self.client_socket.send("Enter transaction type: ".encode("utf-8"))
+        tr_flow = self.client_socket.recv(1024).decode("utf-8").strip()
+
+        self.client_socket.send("Enter category name: ".encode("utf-8"))
+        category = self.client_socket.recv(1024).decode("utf-8").strip()
+
+        self.client_socket.send("Enter value: ".encode("utf-8"))
+        value = self.client_socket.recv(1024).decode("utf-8").strip()
+
+        id_data = self.financial_manager.add_data(
+            tr_flow,
+            category,
+            value,
+            self.user_id
+            )
+        self.client_socket.send(f"Transaction added with ID: {id_data}"
+                                .encode("utf-8"))
+
+    def update_data(self):
+        self.client_socket.send("Enter transaction ID to edit: "
+                                .encode("utf-8"))
+        id_data = int(self.client_socket.recv(1024).decode("utf-8").strip())
+
+        self.client_socket.send(
+            "Enter new transaction type (leave empty to keep current): "
+            .encode("utf-8"))
+        tr_flow = self.client_socket.recv(1024).decode("utf-8").strip() or None
+
+        self.client_socket.send(
+            "Enter new category (leave empty to keep current): "
+            .encode("utf-8"))
+        category = (
+            self.client_socket.recv(1024).decode("utf-8").strip() or None
+            )
+
+        self.client_socket.send(
+            "Enter new value (leave empty to keep current): "
+            .encode("utf-8"))
+        value = self.client_socket.recv(1024).decode("utf-8").strip()
+        value = float(value) if value else None
+        if self.financial_manager.update_data(id_data,
+                                              tr_flow,
+                                              category,
+                                              value):
+            self.client_socket.send("Transaction updated successfully."
+                                    .encode("utf-8"))
+        else:
+            self.client_socket.send("Transaction updated successfully."
+                                    .encode("utf-8"))
+
+    def remove_data(self):
+        self.client_socket.send("Enter category ID to remove: "
+                                .encode("utf-8"))
+        id_data = self.client_socket.recv(1024).decode("utf-8").strip()
+
+        if self.financial_manager.remove_data(id_data):
+            self.client_socket.send("Transaction removed successfully."
+                                    .encode("utf-8"))
+        else:
+            self.client_socket.send("Transaction not found.".encode("utf-8"))
