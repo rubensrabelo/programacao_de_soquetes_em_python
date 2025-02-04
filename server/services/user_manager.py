0f324015fe6262715b7e726adba752ab3e9cda9e
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-02-04 18:07:18 -0300

    Fixing code and adding get_user_id method

--- server/services/user_manager.py
+++ server/services/user_manager.py
@@ -31,14 +31,21 @@ class UserManager():
             return False
         df = pd.read_csv(self.USERS_FILE)
         if df.empty:
-            id = 1
+            id_user = 1
         else:
-            id = df["id"].max() + 1
+            id_user = int(df["id"].max() + 1)
         hashed_password = self.hash_password(password)
         with open(self.USERS_FILE, "a", newline="", encoding="utf-8") as file:
-            file.write(f"{id},{email},{hashed_password}\n")
+            file.write(f"{id_user},{email},{hashed_password}\n")
         return True
 
     def check_user_exists(self, email):
         df = pd.read_csv(self.USERS_FILE)
         return email in df["email"].values
+
+    def get_user_id(self, email):
+        df = pd.read_csv(self.USERS_FILE)
+        user = df[df["email"] == email]
+        if not user.empty:
+            return int(user["id"].values[0])
+        return None
