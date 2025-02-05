9df676c9008ec7d3428e1ce1bc74587c957ce08a
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-02-05 09:05:23 -0300

    Separating and adding financial functionality to the client

--- client/client.py
+++ client/client.py
@@ -1,12 +1,152 @@
 import socket
 
 
-def run_client():
+def connect_to_server(server_ip="127.0.0.1", port=8000):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_ip = "127.0.0.1"
-    port = 8000
-
     client.connect((server_ip, port))
+    return client
+
+
+def handle_registration(client):
+    response = client.recv(1024).decode("utf-8")
+    print(response)
+    email = input(">> ")
+    client.send(email.encode("utf-8"))
+
+    response = client.recv(1024).decode("utf-8")
+    print(response)
+    password = input(">> ")
+    client.send(password.encode("utf-8"))
+
+    response = client.recv(1024).decode("utf-8")
+    print(response)
+
+
+def handle_login(client):
+    response = client.recv(1024).decode("utf-8")
+    print(response)
+    email = input(">> ")
+    client.send(email.encode("utf-8"))
+
+    response = client.recv(1024).decode("utf-8")
+    print(response)
+    password = input(">> ")
+    client.send(password.encode("utf-8"))
+
+    auth_response = client.recv(1024).decode("utf-8")
+    if auth_response == "denied":
+        print("Login failed. Closing connection.")
+        client.close()
+        return
+
+    print("Login successful!")
+    return client
+
+
+def interact_with_server(client):
+    while True:
+        text = """Enter one of the options: [1] 'Financial' or [2] 'Exit'"""
+        print(text)
+        choice = int(input(">> "))
+        client.send(choice.encode("utf-8")[:1024])
+
+        response = client.recv(1024).decode("utf-8")
+
+        if not response:
+            print("Server did not respond")
+            break
+
+        if choice == "1":
+            handle_financial_manager(client)
+        elif choice == "2":
+            print("Exiting...")
+            break
+        else:
+            print("Invalid option. Try again.")
+
+        # print(f"Received: {response}")
+
+
+def handle_financial_manager(client):
+    while True:
+        response = client.recv(1024).decode("utf-8")
+        if response:
+            print(response)
+            choice = input(">> ")
+            client.send(choice.encode("utf-8"))
+
+            if choice.lower() == "add":
+                handle_add_transaction(client)
+            elif choice.lower() == "edit":
+                handle_remove_transaction(client)
+            elif choice.lower() == "remove":
+                handle_update_transaction(client)
+            elif choice.lower() == "clode":
+                print("Closing connection...")
+                break
+            else:
+                print("Invalid option. Try again.")
+
+
+def handle_add_transaction(client):
+    response = client.recv(1024).decode("utf-8")
+    print(response)
+    tr_flow = input(">> ")
+    client.send(tr_flow.encode("utf-8"))
+
+    response = client.recv(1024).decode("utf-8")
+    print(response)
+    category = input(">> ")
+    client.send(category.encode("utf-8"))
+
+    response = client.recv(1024).decode("utf-8")
+    print(response)
+    value = input(">> ")
+    client.send(value.encode("utf-8"))
+
+    response = client.recv(1024).decode("utf-8")
+    print(response)
+
+
+def handle_update_transaction(client):
+    response = client.recv(1024).decode("utf-8")
+    print(response)
+    id_data = input(">> ")
+    client.send(id_data.encode("utf-8"))
+
+    response = client.recv(1024).decode("utf-8")
+    print(response)
+    tr_flow = input(">> ")
+    client.send(tr_flow.encode("utf-8"))
+
+    response = client.recv(1024).decode("utf-8")
+    print(response)
+    category = input(">> ")
+    client.send(category.encode("utf-8"))
+
+    response = client.recv(1024).decode("utf-8")
+    print(response)
+    value = input(">> ")
+    client.send(value.encode("utf-8"))
+
+    response = client.recv(1024).decode("utf-8")
+    print(response)
+
+
+def handle_remove_transaction(client):
+    response = client.recv(1024).decode("utf-8")
+    print(response)
+    id_data = input(">> ")
+    client.send(id_data.encode("utf-8"))
+    response = client.recv(1024).decode("utf-8")
+    print(response)
+
+
+def run_client():
+    # server_ip = "127.0.0.1"
+    # port = 8000
+
+    client = connect_to_server()
 
     try:
         response = client.recv(1024).decode("utf-8")
@@ -15,51 +155,12 @@ def run_client():
         client.send(choice.encode("utf-8"))
 
         if choice.lower() == "no":
-            response = client.recv(1024).decode("utf-8")
-            print(response)
-            email = input(">> ")
-            client.send(email.encode("utf-8"))
+            handle_registration(client)
 
-            response = client.recv(1024).decode("utf-8")
-            print(response)
-            password = input(">> ")
-            client.send(password.encode("utf-8"))
+        handle_login(client)
 
-            response = client.recv(1024).decode("utf-8")
-            print(response)
-
-        response = client.recv(1024).decode("utf-8")
-        print(response)
-        email = input(">> ")
-        client.send(email.encode("utf-8"))
-
-        response = client.recv(1024).decode("utf-8")
-        print(response)
-        password = input(">> ")
-        client.send(password.encode("utf-8"))
-
-        auth_response = client.recv(1024).decode("utf-8")
-        if auth_response == "denied":
-            print("Login failed. Closing connection.")
-            client.close()
-            return
-
-        print("Login successful!")
-
-        while True:
-            msg = input("Enter a message: ")
-            client.send(msg.encode("utf-8")[:1024])
-
-            response = client.recv(1024).decode("utf-8")
-
-            if not response:
-                print("Server did not respond")
-                break
-
-            if response.lower() == "closed":
-                break
-
-            print(f"Received: {response}")
+        if client:
+            interact_with_server(client)
     except Exception as e:
         print(f"Error: {e}")
     finally:
